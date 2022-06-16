63331e904e45a9ba29de30dbda74b353bb99b5da
RomanParaskiva <parasta2018@gmail.com>
2022-06-16 15:54:02 +0300

    done employees, customers and orders

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids'
+
+import { customersData, customersGrid } from '../data/dummy'
+
+import { Header } from '../components'
+
 
 const Customers = () => {
-  return ( 
-    <div>Customers</div>
+  return (
+    <div className='m-2 mt-10 md:m-10 p-2 pt-10 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        width={'auto'}
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={['Delete', 'Add']}
+        editSettings={{ allowDeleting: true, allowEditing: true, allowAdding: true}}
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Page, Edit, Toolbar, Selection, Sort, Filter]} />
+      </GridComponent>
+    </div>
   )
 }
 
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Toolbar, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject, Search } from '@syncfusion/ej2-react-grids'
+
+import { employeesData, employeesGrid } from '../data/dummy'
+import { Header } from '../components'
 
 const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className='m-2 mt-10 md:m-10 p-2 pt-10 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        width={'auto'}
+        toolbar={['Search']}
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Edit, Toolbar]} />
+      </GridComponent>
+    </div>
   )
 }
 
